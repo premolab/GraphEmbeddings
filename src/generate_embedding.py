885f8233fe8ce662861d9abd4c5a1b9c1af64a76
Stas <tsepa.stas@gmail.com>
2017-07-13 20:39:32 +0300

    uploading dataset

--- src/generate_embedding.py
+++ src/generate_embedding.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # available methods:
     # 'stoc_hist', 'node2vec', 'stoc_opt', 'stocsk_opt',
     # 'bigclam', 'gamma', 'deepwalk', 'svd', 'nmf'
-    methods = ['node2vec']
+    methods = ['deepwalk']
     dimensions = [32]
     graph = load_blog_catalog()
     for method, dimension in product(methods, dimensions):
